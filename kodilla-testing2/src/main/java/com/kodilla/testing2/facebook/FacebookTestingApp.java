5a9bebde7a307963a5c9861e83f7df67fd8baf43
BarcaLS <marcin@sarna.info>
2021-02-04 07:58:50 +0100

    facebook

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -9,9 +9,11 @@ import org.openqa.selenium.support.ui.Select;
 public class FacebookTestingApp {
     public static final String XPATH_COOKIES = "//div[contains(@class, \"_9fiw\")]/button[2]";
     public static final String XPATH_NEW_ACCOUNT = "//div[contains(@class, \"_6ltg\")]/a";
-    public static final String XPATH_DAY = "//span[@data-name=\"birthday_wrapper\"]";
+    public static final String XPATH_DAY = "//select[@name=\"birthday_day\"]";
+    public static final String XPATH_MONTH = "//select[@name=\"birthday_month\"]";
+    public static final String XPATH_YEAR = "//select[@name=\"birthday_year\"]";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://facebook.com");
 
@@ -21,9 +23,17 @@ public class FacebookTestingApp {
         WebElement newAccount = driver.findElement(By.xpath(XPATH_NEW_ACCOUNT));
         newAccount.click();
 
-        while (!driver.findElement(By.xpath(XPATH_DAY)).isDisplayed());
+        Thread.sleep(2000);
         WebElement day = driver.findElement(By.xpath(XPATH_DAY));
         Select selectDay = new Select(day);
-        selectDay.selectByIndex(21);
+        selectDay.selectByValue("21");
+
+        WebElement month = driver.findElement(By.xpath(XPATH_MONTH));
+        Select selectMonth = new Select(month);
+        selectMonth.selectByValue("11");
+
+        WebElement year = driver.findElement(By.xpath(XPATH_YEAR));
+        Select selectYear = new Select(year);
+        selectYear.selectByValue("1981");
     }
 }
